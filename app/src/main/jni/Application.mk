c8808b54a41a6a2b9b175db46772ee4ed4c1ed71
wangnan1 <wangnan1@xiaomi.com>
2018-09-03 00:22:41 +0800

    import into Android studio
    Signed-off-by: wangnan1 <wangnan1@xiaomi.com>

---
+++ app/src/main/jni/Application.mk
@@ -0,0 +1,6 @@
+APP_ABI := armeabi-v7a
+APP_STL := gnustl_static
+APP_CPPFLAGS := -std=c++11 -fexceptions -D__STDC_LIMIT_MACROS
+NDK_TOOLCHAIN_VERSION = 4.9
+APP_PLATFORM := android-9
+APP_ALLOW_MISSING_DEPS=true
